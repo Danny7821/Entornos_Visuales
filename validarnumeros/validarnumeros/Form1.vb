934398802006ffb49564816eb4c5aa5463f1b8e1
Danny <Danny@DANNY>
2020-05-29 11:27:57 -0600

    Ejercicios dessarollados I parcial.

--- validarnumeros/validarnumeros/Form1.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Class Form1
-    Private Sub Btncalcular_Click(sender As Object, e As EventArgs) Handles Btncalcular.Click
-        Dim valor As Int16
-        If valor Then Not IsNumeric(txtvalor)
-
-    End Sub
-End Class
