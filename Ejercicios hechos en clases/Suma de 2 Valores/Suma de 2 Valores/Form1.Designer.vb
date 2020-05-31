891f351a5d81893f2a505b1a26763ff894e5e49a
Danny <Danny@DANNY>
2020-05-31 17:36:27 -0600

    .

---
+++ Ejercicios hechos en clases/Suma de 2 Valores/Suma de 2 Valores/Form1.Designer.vb
@@ -0,0 +1,141 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtvalor1 = New System.Windows.Forms.TextBox()
+        Me.txtvalor2 = New System.Windows.Forms.TextBox()
+        Me.txtresultado = New System.Windows.Forms.TextBox()
+        Me.btncalcular = New System.Windows.Forms.Button()
+        Me.btnlimpiar = New System.Windows.Forms.Button()
+        Me.btnsalir = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(51, 48)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(40, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Valor 1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(230, 48)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(40, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Valor 2"
+        '
+        'txtvalor1
+        '
+        Me.txtvalor1.Location = New System.Drawing.Point(22, 78)
+        Me.txtvalor1.Name = "txtvalor1"
+        Me.txtvalor1.Size = New System.Drawing.Size(100, 20)
+        Me.txtvalor1.TabIndex = 2
+        '
+        'txtvalor2
+        '
+        Me.txtvalor2.Location = New System.Drawing.Point(194, 78)
+        Me.txtvalor2.Name = "txtvalor2"
+        Me.txtvalor2.Size = New System.Drawing.Size(100, 20)
+        Me.txtvalor2.TabIndex = 3
+        '
+        'txtresultado
+        '
+        Me.txtresultado.Location = New System.Drawing.Point(34, 208)
+        Me.txtresultado.Name = "txtresultado"
+        Me.txtresultado.Size = New System.Drawing.Size(100, 20)
+        Me.txtresultado.TabIndex = 4
+        '
+        'btncalcular
+        '
+        Me.btncalcular.Location = New System.Drawing.Point(219, 118)
+        Me.btncalcular.Name = "btncalcular"
+        Me.btncalcular.Size = New System.Drawing.Size(75, 23)
+        Me.btncalcular.TabIndex = 5
+        Me.btncalcular.Text = "Calcular"
+        Me.btncalcular.UseVisualStyleBackColor = True
+        '
+        'btnlimpiar
+        '
+        Me.btnlimpiar.Location = New System.Drawing.Point(219, 170)
+        Me.btnlimpiar.Name = "btnlimpiar"
+        Me.btnlimpiar.Size = New System.Drawing.Size(75, 23)
+        Me.btnlimpiar.TabIndex = 6
+        Me.btnlimpiar.Text = "Limpiar"
+        Me.btnlimpiar.UseVisualStyleBackColor = True
+        '
+        'btnsalir
+        '
+        Me.btnsalir.Location = New System.Drawing.Point(221, 221)
+        Me.btnsalir.Name = "btnsalir"
+        Me.btnsalir.Size = New System.Drawing.Size(75, 23)
+        Me.btnsalir.TabIndex = 7
+        Me.btnsalir.Text = "Salir"
+        Me.btnsalir.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(51, 180)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(55, 13)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Resultado"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(308, 256)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.btnsalir)
+        Me.Controls.Add(Me.btnlimpiar)
+        Me.Controls.Add(Me.btncalcular)
+        Me.Controls.Add(Me.txtresultado)
+        Me.Controls.Add(Me.txtvalor2)
+        Me.Controls.Add(Me.txtvalor1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtvalor1 As TextBox
+    Friend WithEvents txtvalor2 As TextBox
+    Friend WithEvents txtresultado As TextBox
+    Friend WithEvents btncalcular As Button
+    Friend WithEvents btnlimpiar As Button
+    Friend WithEvents btnsalir As Button
+    Friend WithEvents Label3 As Label
+End Class
---
+++ Ejercicios hechos en clases/Suma de 2 Valores/Suma de 2 Valores/Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+    Private Sub btncalcular_Click(sender As Object, e As EventArgs) Handles btncalcular.Click
+        Dim valor1, valor2 As Double
+
+        valor1 = Val(txtvalor1.Text)
+        valor2 = Val(txtvalor2.Text)
+
+        txtresultado.Text = valor1 + valor2
+    End Sub
+
+    Private Sub btnlimpiar_Click(sender As Object, e As EventArgs) Handles btnlimpiar.Click
+        txtresultado.Clear()
+        txtvalor1.Clear()
+        txtvalor2.Clear()
+    End Sub
+
+    Private Sub btnsalir_Click(sender As Object, e As EventArgs) Handles btnsalir.Click
+        End
+    End Sub
+End Class
